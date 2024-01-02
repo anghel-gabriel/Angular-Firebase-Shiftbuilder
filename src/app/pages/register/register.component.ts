fd14e6ebf8ec82713ea175941419cb08557626b6
Anghel Gabriel - Irimia <anghel.ergosum@gmail.com>
2024-01-02 06:29:48 +0200

    register validation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MessageService } from 'primeng/api';
+import {
+  isEmailValid,
+  isPasswordValid,
+  isUsernameValid,
+  isUserEighteenYearsAgo,
+} from '../../utils/validation';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,6 +15,24 @@ import { MessageService } from 'primeng/api';
   providers: [MessageService],
 })
 export class RegisterComponent {
+  email = '';
+  username = '';
+  password = '';
+  confirmPassword = '';
+  firstName = '';
+  lastName = '';
+  birthDate = '';
+  gender = '';
+  activeIndex = 0;
+  checked = false;
+  isLoading = false;
+  genderOptions = [
+    { name: 'Unknown', value: 'unknown' },
+    { name: 'Male', value: 'male' },
+    { name: 'Female', value: 'female' },
+    { name: 'Other', value: 'other' },
+  ];
+
   items: MenuItem[] | undefined = [
     {
       label: 'Login credentials',
@@ -23,39 +48,56 @@ export class RegisterComponent {
 
   constructor(private messageService: MessageService) {}
 
-  show() {
+  showError(message: string) {
     this.messageService.add({
-      severity: 'success',
-      summary: 'Success',
-      detail: 'Message Content',
+      severity: 'error',
+      summary: 'Error',
+      detail: message,
     });
   }
-  email = '';
-  username = '';
-  password = '';
-  confirmPassword = '';
-  firstName = '';
-  lastName = '';
-  birthDate = '';
-  gender = '';
-  activeIndex = 0;
-  checked = false;
-  isLoading = true;
 
   handleNext() {
+    // first step validation
+    if (this.activeIndex === 0) {
+      if (!isEmailValid(this.email)) {
+        this.showError('Please use a valid email address');
+        return;
+      }
+      if (!isUsernameValid(this.username)) {
+        this.showError('Your username must be alphanumeric');
+        return;
+      }
+      if (!isPasswordValid(this.password)) {
+        this.showError('Your password must respect the requested format');
+        return;
+      }
+      if (this.password !== this.confirmPassword) {
+        this.showError('Your passwords must match');
+        return;
+      }
+    }
+    // second step validation
+    if (this.activeIndex === 1) {
+      if (this.firstName.length < 6 || this.lastName.length < 6) {
+        this.showError(
+          'First name and last name must be at least 6 characters long'
+        );
+        return;
+      }
+      if (!this.birthDate || !isUserEighteenYearsAgo(this.birthDate)) {
+        this.showError(
+          'You must be at least 18 years old in order to register'
+        );
+        return;
+      }
+    }
+
     if (this.activeIndex !== 2) this.activeIndex++;
   }
 
   handlePrevious() {
     if (this.activeIndex !== 0) this.activeIndex--;
   }
-
-  options = [
-    { name: 'Unknown', value: 'unknown' },
-    { name: 'Male', value: 'male' },
-    { name: 'Female', value: 'female' },
-    { name: 'Other', value: 'other' },
-  ];
 
   onActiveIndexChange(event: number) {
     this.activeIndex = event;
--- src/app/utils/validation.ts
+++ src/app/utils/validation.ts
@@ -1,14 +1,25 @@
-export const isEmailValid = (email: string) => {
+export function isEmailValid(email: string) {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  return regex.test(email);
-};
-
-function isPasswordValid(password: string) {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/;
-  return regex.test(password);
+  return email.match(regex);
 }
 
-function isUsernameValid(username: string) {
+export function isPasswordValid(password: string) {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/;
+  return password.match(regex);
+}
+
+export function isUsernameValid(username: string) {
   const regex = /^[A-Za-z0-9]+$/;
-  return regex.test(username);
+  return username.match(regex);
+}
+
+export function isUserEighteenYearsAgo(date: string) {
+  const givenDate = new Date(date);
+  const currentDate = new Date();
+  const eighteenYearsAgo = new Date(
+    currentDate.getFullYear() - 18,
+    currentDate.getMonth(),
+    currentDate.getDate()
+  );
+  return givenDate <= eighteenYearsAgo;
 }
